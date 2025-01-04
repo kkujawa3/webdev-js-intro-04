16e7c1d16e601724307722ed10300ee8a102099c
unknown <kimberllee.al@gmail.com>
2025-01-04 15:41:01 -0500

    Completed JS assignments part 1 and 2.

--- js/assignment.js
+++ js/assignment.js
@@ -2,16 +2,24 @@
 
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
-const submissionBtn = document.getElementById("submission-btn");
+const submissionBtn = document.getElementById("submission-btn")
+
+const responseParagraph = document.getElementById("response")
 
 // create a mutable variable called age and do not assign it a value.
-
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
-
+    if (age >= 21) {
+        responseParagraph.textContent = "You can vote and purchase alcohol.";
+    } else if (age >= 18 && age <= 20) {
+        responseParagraph.textContent = "You can vote, but you cannot purchase alcohol.";        
+    } else {
+        responseParagraph.textContent = "You cannot vote and you cannot purchase alcohol.";
+    }
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,68 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let healthPoints = 2;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+// If Statements
+
+let dogBreed = false;
+
+if (dogBreed === true) {
+    console.log("Chiweenie");
+}
+
+let myFootball = false;
+
+if (myFootball === true) {
+    console.log("Touchdown");
+}
+
+// If/else statements
+
+let dogBreed = false;
+
+if (dogBreed === true) {
+    console.log("Chiweenie");
+} else {
+    console.log("This is a cat breed");    
+}
+
+let myFootball = false;
+
+if (myFootball === true) {
+    console.log("Touchdown");
+} else {
+    console.log("This is soccer");    
+}
+
+// if/else if/else statements
+
+let albumSongs = 10;
+
+if (albumSongs >= 12) {
+    console.log("This album is awesome, bonus songs added");
+} else if (albumSongs < 12 && albumSongs >= 10) {
+    console.log("Great listening");
+} else {
+    console.log("Don't buy album");    
+}   
+
+let recipeBook = 15;
+
+if (recipeBook >= 20) {
+    console.log("Surprise recipes included");
+} else if (recipeBook < 20 && recipeBook >= 15) {
+    console.log("Keep book, yummy recipes");
+} else {
+    console.log("Share book with others");
+}
+*/
